96ed8d1a1c748cc85aaaf6b49650a4176b5ba5e6
Byron <jamesblocker81@gmail.com>
2018-04-02 09:18:34 -0700

    morning js exercise

---
+++ checkForSpace.js
@@ -0,0 +1,14 @@
+function checkForSpace(str) {
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] === "a") {
+            if (str[i + 4] === "b" || str[i - 4] === "b") {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+console.log('------------')
+console.log(checkForSpace("lane borrowed"))
+console.log(checkForSpace("about a boy"))
